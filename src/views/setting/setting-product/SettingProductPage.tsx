5542fcb2cfbacb07df95dd7d888ea1f1720f12c1
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-31 11:31:05 +0700

    create new component view of setting-product

---
+++ src/views/setting/setting-product/SettingProductPage.tsx
@@ -0,0 +1,16 @@
+import {Fragment} from "react";
+import {Card, CardHeader, CardTitle} from "reactstrap";
+
+const SettingProductPage = () => {
+    return (
+        <Fragment>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Pengaturan Produk</CardTitle>
+                </CardHeader>
+            </Card>
+        </Fragment>
+    )
+}
+
+export default SettingProductPage
