7e9c5c12711b4298bc92efa3acec28586ac11648
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-31 11:30:57 +0700

    create new component view of setting-company

---
+++ src/views/setting/setting-company/SettingCompanyPage.tsx
@@ -0,0 +1,16 @@
+import {Fragment} from "react";
+import {Card, CardHeader, CardTitle} from "reactstrap";
+
+const SettingCompanyPage = () => {
+    return (
+        <Fragment>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Pengaturan Perusahaan</CardTitle>
+                </CardHeader>
+            </Card>
+        </Fragment>
+    )
+}
+
+export default SettingCompanyPage
