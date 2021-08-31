191368525eca6fff60e32c731904bae21d631109
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-31 11:30:37 +0700

    create new component view of setting

---
+++ src/views/setting/setting/SettingPage.tsx
@@ -0,0 +1,16 @@
+import {Fragment} from "react";
+import {Card, CardHeader, CardTitle} from "reactstrap";
+
+const SettingPage = () => {
+    return (
+        <Fragment>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Pengaturan</CardTitle>
+                </CardHeader>
+            </Card>
+        </Fragment>
+    )
+}
+
+export default SettingPage
