6617076a7c7255c5b57cf1f8f01efb908d5485bb
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-11 11:54:51 +0700

    [core] fix formatDateToCommonFormat, bug in parse date

--- src/utility/date-format-util.ts
+++ src/utility/date-format-util.ts
@@ -2,7 +2,7 @@ export const formatDateToCommonFormat = (date: Date) => {
 
     const formatted = new Intl.DateTimeFormat("in-ID").format(date)
 
-    return date.getDate() + "-" + (date.getMonth() + 1).toString().padStart(2, "0") + "-" + date.getFullYear()
+    return date.getDate().toString().padStart(2, "0") + "-" + (date.getMonth() + 1).toString().padStart(2, "0") + "-" + date.getFullYear()
 }
 
 export const dateOfDayMonthYear = (date: Date) => {
