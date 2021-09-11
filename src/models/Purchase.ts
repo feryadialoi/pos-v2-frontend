75598ab158e4e5a118cd0ee788157c729149734e
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-11 11:49:18 +0700

    [core] add model Purchase content

--- src/models/Purchase.ts
+++ src/models/Purchase.ts
@@ -1,4 +1,27 @@
+import {PurchaseOrder} from "./PurchaseOrder";
+import {Supplier} from "./Supplier";
+import {PaymentType} from "./PaymentType";
+import {PurchaseStatus} from "./PurchaseStatus";
+
 export interface Purchase {
     id: string
+    purchaseOrder: PurchaseOrder
     code: string
+    entryDate: string
+    dueDate: string
+    supplier: Supplier
+    reference: string
+    note: string
+    attachment: string | null
+    shippingFee: number
+    shippingFeeDescription: string
+    otherFee: number
+    otherFeeDescription: string
+    paymentType: PaymentType
+    discount: number
+    tax: number
+    total: number
+    grandTotal: number
+    status: PurchaseStatus
+
 }
