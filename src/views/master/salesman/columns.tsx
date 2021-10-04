d0038f90c24948c362b1d7b25eeb1aa3f2f9be0d
Feryadi Aloi <feryadialoi@gmail.com>
2021-10-04 10:40:43 +0700

    [core] salesman add field to show in webpage

--- src/views/master/salesman/columns.tsx
+++ src/views/master/salesman/columns.tsx
@@ -16,4 +16,25 @@ export const columns: IDataTableColumn<Salesman & { no: number }> [] = [
         sortable: true,
         cell: row => row.name
     },
+    {
+        name: 'Nik',
+        minWidth: '200px',
+        selector: 'nationalIdentificationNumber',
+        sortable: true,
+        cell: row => row.nationalIdentificationNumber
+    },
+    {
+        name: 'Nomor Telepon',
+        minWidth: '200px',
+        selector: 'phone',
+        sortable: true,
+        cell: row => row.phone
+    },
+    {
+        name: 'Alamat',
+        minWidth: '200px',
+        selector: 'address',
+        sortable: true,
+        cell: row => row.address
+    },
 ]
